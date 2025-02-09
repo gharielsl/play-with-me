5420c457b64d8328ac2de030b15d7e4aaba4a2ce
gharielsl <slavinskyariel@gmail.com>
2025-02-09 15:41:53 +0200

    create cards

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -10,8 +10,10 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
-const pages = ['Home', 'Play'];
+const pages = [['Home', '/'], ['Play', '/lobbies']];
 const settings = ['Account', 'Logout'];
 
 function NavBar() {
@@ -36,24 +38,26 @@ function NavBar() {
     return (
         <AppBar position="static" sx={{ backgroundColor: 'var(--color-primary)' }}>
             <Container sx={{ maxWidth: 'unset !important' }}>
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none'
-                        }}
-                    >
-                        LOGO
-                    </Typography>
+                <Toolbar disableGutters sx={{ height: 64 }}>
+                    <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            sx={{
+                                mt: '0.2em',
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                    </div>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
@@ -83,46 +87,40 @@ function NavBar() {
                             sx={{ display: { xs: 'block', md: 'none' } }}
                         >
                             {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                                <MenuItem key={page[0]} onClick={handleCloseNavMenu}>
+                                    <Link className='link mobile' to={page[1]} style={{ textAlign: 'center' }}>{page[0]}</Link>
                                 </MenuItem>
                             ))}
                         </Menu>
                     </Box>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                    </div>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', height: '100%' } }}>
                         {pages.map((page) => (
-                            <Button
-                                key={page}
+                            <Link
+                                to={page[1]}
+                                key={page[0]}
                                 onClick={handleCloseNavMenu}
-                                sx={{ 
-                                    my: 2, 
-                                    color: 'white', 
-                                    display: 'block',
-                                    ':hover': {
-                                        color: 'var(--color-secondary)'
-                                    }
-                                }}
+                                className="link"
                             >
-                                {page}
-                            </Button>
+                                {page[0]}
+                            </Link>
                         ))}
                     </Box>
                     <Box sx={{ flexGrow: 0 }}>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function scrollToElement(el: string) {
     if (el) {
@@ -12,8 +12,25 @@ function scrollToElement(el: string) {
 }
 
 function Home() {
+    const stepRowRef = useRef<HTMLDivElement>(null);
+    const steps = Array.from({ length: 4 }, () => useRef<HTMLDivElement>(null));
+    const infoReady = useRef<boolean>(false);
+
     useEffect(() => {
+        const observer = new IntersectionObserver(() => {
+            if (infoReady.current) {
+                steps.forEach(({ current }) => {
+                    current?.classList.add("visible");
+                });
+            }
+        });
+
+        if (stepRowRef.current) {
+            observer.observe(stepRowRef.current);
+        }
+
         setTimeout(() => {
+            infoReady.current = true;
             scrollToElement(location.hash);
         }, 1100);
     }, []);
@@ -24,10 +41,10 @@ function Home() {
                 <h1>Play With Me</h1>
                 <div className="short-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
                 <div className="start-btn-container">
-                    <Link className="start-btn play" to="/lobbies">Play</Link>
-                    <Link 
-                        onClick={() => scrollToElement('#learn_more')} 
-                        className="start-btn about" 
+                    <Link className="start-btn play-btn" to="/lobbies">Play</Link>
+                    <Link
+                        onClick={() => scrollToElement('#learn_more')}
+                        className="start-btn about-btn"
                         to="#learn_more"
                     >
                         Learn more
@@ -35,10 +52,65 @@ function Home() {
                 </div>
             </div>
             <div className="info" id="learn_more">
-                <h3>Play With Me</h3>
+                <h2>Play With Me</h2>
                 <div className="about">
                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
                     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt corporis explicabo quisquam iusto esse. Libero voluptate deleniti recusandae consequuntur officiis inventore porro error officia corrupti eveniet, sit architecto minus facilis.</div>
+                </div>
+                <div className="features">
+                    <h2>
+                        Let's see how it works
+                    </h2>
+                    <div ref={stepRowRef} className="step-row">
+                        <div className="step-row-half">
+                            <div ref={steps[0]} className="step s1">
+                                <img src="https://avatars.githubusercontent.com/u/104318152?s=400&u=9b775b3e0aea34bf3585af884a3e3033524e44db&v=4">
+                                </img>
+                                <div>
+                                    <h3>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                </div>
+                            </div>
+                            <div ref={steps[1]} className="step s2">
+                                <img src="https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg">
+                                </img>
+                                <div>
+                                    <h3>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="step-row-half">
+                            <div ref={steps[2]} className="step s3">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK5CqiQQDLVEVd_mEtfKpqF8MTZj0SqiEEWg&s">
+                                </img>
+                                <div>
+                                    <h3>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                </div>
+                            </div>
+                            <div ref={steps[3]} className="step s4">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCsqRYLAFDdL4Ix_AHai7kNVyoPV9Ssv1xg&s">
+                                </img>
+                                <div>
+                                    <h3>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae ad doloremque est eaque voluptatum minus veritatis, excepturi minima nihil enim impedit in deserunt dolores tenetur tempore culpa at. Magni?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
