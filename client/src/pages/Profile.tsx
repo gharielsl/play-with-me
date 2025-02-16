b39ed38ffccfbfa77683fce3e17c72815d52a7ff
gharielsl <slavinskyariel@gmail.com>
2025-02-16 11:26:13 +0200

    add edit image

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,7 @@
 import { CircularProgress, styled, TextField } from "@mui/material";
 import "./Profile.css";
 import { useUser } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import Cookie from 'js-cookie';
 import Success from "../components/Success";
@@ -19,7 +19,7 @@ function textFieldStyle(error: boolean) {
                 borderColor: secondary
             },
             '&.Mui-focused fieldset': {
-                borderColor: border
+                borderColor: secondary
             }
         },
         '& .MuiInputLabel-root': {
@@ -32,21 +32,39 @@ function textFieldStyle(error: boolean) {
 }
 
 export default function Profile() {
-    const { user } = useUser();
+    const { user, login } = useUser();
     const navigate = useNavigate();
+    const location = useLocation();
     const [displayName, setDisplayName] = useState('');
     const [bio, setBio] = useState('');
+    const [imgState, setImgState] = useState<'none' | 'edit' | 'confirm'>('none');
+    const prevImgState = useRef<'none' | 'edit' | 'confirm'>('none');
     const [state, setState] = useState<'none' | 'saving' | 'saved' | 'error'>('none');
+    const [image, setImage] = useState('');
     const displayNameRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         setDisplayName(user?.displayName || '');
         setBio(user?.bio || '');
+        setImage(user?.profileImg || '');
     }, [user]);
+
+    useEffect(() => {
+        prevImgState.current = imgState;
+    }, [imgState]);
 
     let displayNameError = '';
     if (displayName.length < 3 || displayName.length > 60) {
         displayNameError = 'Name must be at least 3 characters long';
+    }
+
+    function exit() {
+        const params = new URLSearchParams(location.search);
+        if (params.get('justCreated') === 'true') {
+            navigate('/');
+        } else {
+            navigate(-1);
+        }
     }
 
     async function save() {
@@ -65,14 +83,53 @@ export default function Profile() {
             },
             body: JSON.stringify({
                 displayName,
-                bio
+                bio,
+                profileImg: image
             })
         });
         if (resp.ok) {
             setState('saved');
+            if (user) {
+                login({ ...user, profileImg: image });
+            }
+            setTimeout(() => setState('none'), 1000);
         } else {
             setState('error');
         }
+    }
+
+    function uploadImage() {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+
+        input.addEventListener('change', () => {
+            const file = input.files?.[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    if (e.target?.result) {
+                        setImage(e.target.result as string);
+                        setImgState('confirm');
+                    }
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+
+        input.click();
+    }
+
+    function clickEditImage() {
+        if (!user || user.isGuest) {
+            return;
+        }
+        setImgState('edit');
+    }
+
+    function cancelImgUpdate() {
+        setImage(user?.profileImg || '');
+        setImgState('none');
     }
 
     return (
@@ -80,31 +137,59 @@ export default function Profile() {
             <div className="settings">
                 <h2>Profile</h2>
                 <div className="profile-form">
-                    <div className="profile-image">
+                    {
                         {
-                            user?.profileImg ?
-                                <img src={user?.profileImg} alt="profile"></img> :
-                                <i className="bi bi-person-fill"></i>
-                        }
-                        <i className="bi bi-pencil-fill edit"></i>
-                    </div>
+                            none: (
+                                <div onClick={clickEditImage} className={`profile-image ${prevImgState.current !== 'none' && 'anim'}`}>
+                                    {
+                                        image ?
+                                            <img src={image} alt="profile"></img> :
+                                            <i className="bi bi-person-fill"></i>
+                                    }
+                                    {
+                                        user && !user.isGuest && <i className="bi bi-pencil-fill edit"></i>
+                                    }
+                                </div>
+                            ),
+                            edit: (
+                                <div onClick={uploadImage} className={`profile-img-open ${prevImgState.current === 'none' && 'anim'}`}>
+                                    <div className="profile-img-drag">
+                                        <i className="bi bi-cloud-arrow-up"></i>
+                                    </div>
+                                    <div onClick={cancelImgUpdate} className="cancel"><i className="bi bi-x"></i></div>
+                                </div>
+                            ),
+                            confirm: (
+                                <div className="profile-confirm">
+                                    <img src={image}></img>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', position: 'absolute', left: 0, bottom: 0, right: 1, backgroundColor: '#a5a5a5a5', paddingBottom: 2 }}>
+                                        <div onClick={() => setImgState('none')} className="profile-confirm-btn">Confirm</div>
+                                        <div onClick={() => setImgState('edit')} className="profile-confirm-btn">Retry</div>
+                                    </div>
+                                </div>
+                            )
+                        }[imgState]
+                    }
 
-                    <TextField
-                        inputRef={displayNameRef}
-                        value={displayName}
-                        onChange={(ev) => setDisplayName(ev.target.value)}
-                        disabled={!user || user?.isGuest}
-                        label="Display name"
-                        inputProps={{
-                            maxLength: 60
-                        }}
-                        sx={{
-                            flex: 1,
-                            ml: '16px',
-                            ...textFieldStyle(!!displayNameError)
-                        }}
-                    />
-                    <div className="counter-profile">{displayName.length} / 60</div>
+                    <div className="profile-name-container">
+                        <TextField
+                            inputRef={displayNameRef}
+                            value={displayName}
+                            onChange={(ev) => setDisplayName(ev.target.value)}
+                            disabled={!user || user?.isGuest}
+                            label="Display name"
+                            inputProps={{
+                                maxLength: 60
+                            }}
+                            sx={{
+                                width: 'calc(100% - 16px)',
+                                ml: '16px',
+                                mt: '4px',
+                                ...textFieldStyle(!!displayNameError)
+                            }}
+                        />
+                        <div className="counter-profile">{displayName.length} / 60</div>
+                    </div>
                     {displayNameError && <div className="input-error">{displayNameError}</div>}
 
                 </div>
@@ -118,7 +203,7 @@ export default function Profile() {
                     }}
                 />
 
-                <div style={{ width: '100%', position: 'relative' }}>
+                <div style={{ position: 'relative' }}>
                     <TextField
                         value={bio}
                         onChange={(ev) => setBio(ev.target.value)}
@@ -126,7 +211,7 @@ export default function Profile() {
                         multiline
                         rows={4}
                         label="Bio"
-                        sx={{ my: '12px', width: '100%' }}
+                        sx={{ my: '12px', width: '100%', ...textFieldStyle(false) }}
                         inputProps={{ maxLength: 400 }}
                     />
                     <div className="counter-profile bottom">{bio.length} / 400</div>
@@ -151,7 +236,7 @@ export default function Profile() {
                             }[state]
                         }
                     </div>
-                    <div onClick={() => navigate(-1)} className="save discard">Discard</div>
+                    <div onClick={exit} className="save discard">Discard</div>
                 </div>
             </div>
         </div>
